2337182f422a00e68b9fa8986fa234df6549a422
Dilip <dilip_agheda@yahoo.com>
2019-02-23 21:08:43 +1100

    Implemented all routes and CRUD operations, Tested and working!! Yay!!

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* Declare all constants and variables here */
 const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
@@ -5,23 +6,26 @@ var routes = require('./routes/index');
 var books = require('./routes/books');
 const app = express();
 const path = require('path');
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static('public'));
+
+/* Setup view engine and views folder */
 app.set('view engine','pug');
 app.set('views','views');
 
-
-
+/* Define all middleware functions here */
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
+/* Add routes */
 app.use('/', routes);
 app.use('/books', books);
 
-
-// catch 404
+// Final catch 404 function
 app.use(function(req, res, next) {
-    res.send("error..hahaha!");
-  });
+    res.status(404).render("page_not_found",{title:"Page not found"});
+});
 
-
+/* Start the server only if sync to database is successful */
 sequelize.sync()
         .then(()=>app.listen(3000))
-        .catch(error=>console.log(error));
+        .catch(error=>{
+            res.status(500).render("error",{title:"Server Error"});
+        });
--- routes/books.js
+++ routes/books.js
@@ -1,3 +1,4 @@
+/* Declare all constants and variables here */
 var express = require('express');
 var router = express.Router();
 const Book = require('../models/book');
@@ -6,15 +7,17 @@ const Book = require('../models/book');
 router.get('/', function(req, res, next) {
   Book.findAll()
     .then(books =>{
-      res.render("index",{books});
+      res.render("index",{books:books, title:"All books"});
     })
-    .catch(error=>console.log(error));
+    .catch(error=>{
+      res.render("error",{title:"Server Error"});
+    });
   
 });
 
 /* GET method for /books/new route */
 router.get('/new', function(req, res, next) {
-  res.render("new-book",{book:{},title:"New Book"});
+  res.render("new-book",{book:{},title:"Create New Book"});
 });
 
 
@@ -32,14 +35,69 @@ router.post('/new', function(req, res, next) {
       }
     })
     .catch(function(error){
-        res.send(500, error);
+        res.status(500).render("error",{title:"Server Error"});
     });
   
 });
 
 /* GET method for /books/:id route */
 router.get('/:id', function(req, res, next) {
-  res.render("update-book",{book:{},title:"Update Book"});
+
+  Book.findByPk(req.params.id)
+    .then(book=>{
+      if(book){
+        res.render("update-book",{book:book,title:"Update Book"});
+      }else{
+        res.status(404).render("page-not-found",{title:"Page not found"});
+      }
+    })
+    .catch(function(error){
+      res.status(500).render("error",{title:"Server Error"});
+    });
+  
+});
+
+/* POST method for /books/:id route */
+router.post('/:id', function(req, res, next) {
+  Book.findByPk(req.params.id)
+    .then(book=>{
+      if(book){
+        return book.update(req.body);
+      }else{
+        res.status(404).render("page-not-found",{title:"Page not found"});
+      }
+    })
+    .then(()=>{
+      res.redirect("/");
+    })
+    .catch(function(error){
+      if(error.name === "SequelizeValidationError") {
+        res.render("update-book", {book: {...req.body,id:req.params.id}, errors: error.errors, title: "Update Book"});
+      } else {
+        throw error;
+      }
+    })
+    .catch(function(error){
+      res.status(500).render("error",{title:"Server Error"});
+    });
+});
+
+/* POST method for /books/:id/delete route */
+router.post('/:id/delete', function(req, res, next) {
+  Book.findByPk(req.params.id)
+  .then(book=>{
+    if(book){
+      return book.destroy();
+    }else{
+      res.status(404).render("page-not-found",{title:"Page not found"});
+    }
+  })
+  .then(()=>{
+    res.redirect("/");
+  })
+  .catch(function(error){
+    res.status(500).render("error",{title:"Server Error"});
+  });
 });
 
 module.exports = router;
